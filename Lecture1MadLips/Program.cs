1583d4d07c2102da2fe223000071f9a54d500f60
bobrikovec <ondrej.cejka@student.gymspit.cz>
2025-09-17 09:29:20 +0200

    Add Lecture1MadLips

---
+++ Lecture1MadLips/Program.cs
@@ -0,0 +1,11 @@
+﻿//Napiš program, který se zeptá na barvu, vlastnost, zvíře, činnost a vypíše dohromady větu
+using System;
+Console.WriteLine("Zadej barvu:");
+string color = Console.ReadLine();
+Console.WriteLine("Zadej vlastnost:");
+string adjective = Console.ReadLine();
+Console.WriteLine("Zadej zvíře:");
+string animal = Console.ReadLine();
+Console.WriteLine("Zadej činnost:");
+string activity = Console.ReadLine();
+Console.WriteLine($"Bylo nebylo, v jedné {color} zemi žil {adjective} {animal}, který rád {activity}.");
